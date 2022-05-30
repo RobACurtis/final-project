d03acfd70c3d7515f65e81b53eddbca079dcbe7d
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-30 22:58:44 +0000

    set up database and added images

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,34 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "users" (
+	"userId" serial NOT NULL,
+	"username" TEXT NOT NULL,
+	"hashedPassword" TEXT NOT NULL,
+	"email" TEXT NOT NULL,
+	"location" TEXT NOT NULL,
+  "createdAt" timestamp with time zone NOT NULL,
+	"profileImageUrl" TEXT NOT NULL DEFAULT '/profileImage.jpg',
+	"coverImageUrl" TEXT NOT NULL DEFAULT '/coverImage.jpg',
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "photos" (
+	"photoId" serial NOT NULL,
+	"userId" serial NOT NULL,
+	"imageUrl" TEXT NOT NULL,
+  "createdAt" timestamp with time zone NOT NULL,
+	CONSTRAINT "photos_pk" PRIMARY KEY ("photoId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "photos" ADD CONSTRAINT "photos_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
