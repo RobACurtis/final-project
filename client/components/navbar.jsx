59c06dbb9e69dceec1b25ee38dd5ff0ea4d6fe79
Rob Curtis <Robanthonycurtis@gmail.com>
2022-05-31 02:10:37 +0000

    in progress

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Navbar(props) {
   return (
-  <nav className="navbar navbar-expand-lg bg-dark py-3 fixed-top">
+  <nav className="navbar navbar-expand-lg bg-dark py-3 fixed-top opacity-75">
     <div className="container-fluid">
       <img className="logo" src="/logo.svg" alt="surfer logo" />
     </div>
--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -2,29 +2,44 @@ import React from 'react';
 // import HelloWorld from '../components/hello-world';
 import Navbar from '../components/navbar';
 
-export default function Home(props) {
-  return (
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageId: 0
+    };
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+
+    }, 3000);
+  }
+
+  render() {
+    return (
     <div>
       <Navbar />
       <div id="home-page">
         <div className="hero-image">
-          <img className="img-homepage" src="/images/example1.jpg" alt="surfing"/>
-            <img className="img-homepage" src="/images/example2.jpg" alt="surfing"/>
-              <img className="img-homepage" src="/images/example3.jpg" alt="surfing"/>
-                <img className="img-homepage" src="/images/example7.jpg" alt="surfing"/>
-                </div>
-                <div id="explore" className="container-modal center">
-                  <div id="explore-text" className="row center">
-                    <div id="div" className="column-full center">
-                      <h1 className="">Find your inspiration. <br/></h1>
-                      <h3 className=" join mt-4">Join the Surfr community, home to surf <br/> photographers all over the world.</h3>
-                      <div>
-                        <button id="button" type="button" className="mt-5 btn btn-light btn-lg">Explore</button>
-                      </div>
-                    </div>
-                  </div>
+          <img id="0" className="img-homepage" src="/images/example1.jpg" alt="surfing"/>
+          <img id="1" className="img-homepage" src="/images/example2.jpg" alt="surfing"/>
+          <img id="2" className="img-homepage" src="/images/example3.jpg" alt="surfing"/>
+          <img id="3" className="img-homepage" src="/images/example7.jpg" alt="surfing"/>
+          </div>
+          <div id="explore" className="container-modal center">
+            <div id="explore-text" className="row center">
+              <div id="div" className="column-full center">
+                <h1 className="">Find your inspiration. <br/></h1>
+                <h3 className=" join mt-4">Join the Surfr community, home to surf <br/> photographers all over the world.</h3>
+                <div>
+                  <button id="button" type="button" className="mt-5 btn btn-light btn-lg">Explore</button>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
     </div>
-  );
+    );
+  }
 }
