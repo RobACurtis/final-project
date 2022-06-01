bff81fae1373a68f98a9e27a9b83f9e362f46958
Rob Curtis <Robanthonycurtis@gmail.com>
2022-06-01 00:25:17 +0000

    completed issue 1 user can view gallery

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -46,21 +46,21 @@ export default class Home extends React.Component {
     <>
       <Navbar opacity='opacity-75' />
       <div id="home-page">
-         <div className="hero-image">
-         {imgList}
-          </div>
-          <div id="explore" className="overlay-homepage center">
-            <div id="explore-text" className="d-flex center">
-              <div className="center">
-                <h1 className="home-title">Find your inspiration. <br/></h1>
-                <h3 className=" home-subtitle mt-4">Join the Surfr community, home to surf <br/> photographers all over the world.</h3>
-                <div>
-                  <a href="#explore" type="button" className="mt-5 btn btn-light btn-lg button">Explore</a>
-                </div>
+        <div className="hero-image">
+        {imgList}
+        </div>
+        <div id="explore" className="overlay-homepage center">
+          <div id="explore-text" className="d-flex center">
+            <div className="center">
+              <h1 className="home-title">Find your inspiration.</h1>
+              <h3 className="home-subtitle mt-4">Join the Surfr community, home to surf <br/> photographers all over the world.</h3>
+              <div>
+                <a href="#explore" className="mt-5 btn btn-light btn-lg button">Explore</a>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </>
     );
   }
--- database/data.sql
+++ database/data.sql
@@ -4,13 +4,13 @@ values ('John Johnson', '1234', 'john_johnson@surfr.com', 'Sydney, Australia', n
 
 insert into "photos" ("userId", "imageUrl", "createdAt")
 values (1, '/images/example1.jpg', now()),
-      (1, '/images/example2.jpg', now()),
-      (1, '/images/example3.jpg', now()),
-      (2, '/images/example4.jpg', now()),
-      (1, '/images/example5.jpg', now()),
-      (2, '/images/example6.jpg', now()),
-      (2, '/images/example7.jpg', now()),
-      (1, '/images/example8.jpg', now()),
-      (2, '/images/example9.jpg', now()),
-      (2, '/images/example10.jpg', now()),
-      (2, '/images/example11.jpg', now());
+       (1, '/images/example2.jpg', now()),
+       (1, '/images/example3.jpg', now()),
+       (2, '/images/example4.jpg', now()),
+       (1, '/images/example5.jpg', now()),
+       (2, '/images/example6.jpg', now()),
+       (2, '/images/example7.jpg', now()),
+       (1, '/images/example8.jpg', now()),
+       (2, '/images/example9.jpg', now()),
+       (2, '/images/example10.jpg', now()),
+       (2, '/images/example11.jpg', now());
